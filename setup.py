d064e157e25539018f6c6f95d6642dce2ffdd05c
Matthew Forrest <mforrest00@gmail.com>
2019-08-15 20:14:32 -0500

    Adding setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+    name='vassilakis_sra',
+    packages=['vassilakis_sra'],
+    version='0.1',
+    description='Implementation of roughness calculation model by Pantelis Vassilakis',
+    author='Matthew Forrest',
+    author_email='mforrest00@gmail.com',
+    url='https://github.com/MForrest00/vassilakis_sra',
+    download_url='https://github.com/MForrest00/vassilakis_sra/tarball/0.1',
+    keysword=['sound', 'roughness', 'spectral', 'analysis'],
+    classifiers=[],
+)
